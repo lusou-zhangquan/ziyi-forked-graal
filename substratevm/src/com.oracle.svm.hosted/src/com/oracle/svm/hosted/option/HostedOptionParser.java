oracle/graal

Parse pointsto options for standalone pointsto
The standalone pointsto need to take the input options. The options and
parsing should follow the same way as native image's hosted options.
So we extract the commom part of SubstrateOptionsParser to a new project
where both pointsto and svm can share.

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/option/HostedOptionParser.java
@@ -24,7 +24,7 @@
  */
 package com.oracle.svm.hosted.option;
 
-import static com.oracle.svm.core.option.SubstrateOptionsParser.BooleanOptionFormat.PLUS_MINUS;
+import static com.oracle.svm.common.option.CommonOptionParser.BooleanOptionFormat.PLUS_MINUS;
 import static com.oracle.svm.core.util.VMError.shouldNotReachHere;
 
 import java.util.ArrayList;
@@ -59,30 +59,28 @@ public class HostedOptionParser implements HostedOptionProvider {
     }
 
     public static void collectOptions(ServiceLoader<OptionDescriptors> optionDescriptors, EconomicMap<String, OptionDescriptor> allHostedOptions,
-                    EconomicMap<String, OptionDescriptor> allRuntimeOptions) {
-        for (OptionDescriptors optionDescriptor : optionDescriptors) {
-            for (OptionDescriptor descriptor : optionDescriptor) {
-                String name = descriptor.getName();
+                                      EconomicMap<String, OptionDescriptor> allRuntimeOptions) {
+        SubstrateOptionsParser.collectOptions(optionDescriptors, descriptor -> {
+            String name = descriptor.getName();
 
-                if (descriptor.getDeclaringClass().getAnnotation(Platforms.class) != null) {
-                    throw UserError.abort("Options must not be declared in a class that has a @%s annotation: option %s declared in %s",
-                                    Platforms.class.getSimpleName(), name, descriptor.getDeclaringClass().getTypeName());
-                }
+            if (descriptor.getDeclaringClass().getAnnotation(Platforms.class) != null) {
+                throw UserError.abort("Options must not be declared in a class that has a @%s annotation: option %s declared in %s",
+                        Platforms.class.getSimpleName(), name, descriptor.getDeclaringClass().getTypeName());
+            }
 
-                if (!(descriptor.getOptionKey() instanceof RuntimeOptionKey)) {
-                    OptionDescriptor existing = allHostedOptions.put(name, descriptor);
-                    if (existing != null) {
-                        throw shouldNotReachHere("Option name \"" + name + "\" has multiple definitions: " + existing.getLocation() + " and " + descriptor.getLocation());
-                    }
-                }
-                if (!(descriptor.getOptionKey() instanceof HostedOptionKey)) {
-                    OptionDescriptor existing = allRuntimeOptions.put(name, descriptor);
-                    if (existing != null) {
-                        throw shouldNotReachHere("Option name \"" + name + "\" has multiple definitions: " + existing.getLocation() + " and " + descriptor.getLocation());
-                    }
+            if (!(descriptor.getOptionKey() instanceof RuntimeOptionKey)) {
+                OptionDescriptor existing = allHostedOptions.put(name, descriptor);
+                if (existing != null) {
+                    throw shouldNotReachHere("Option name \"" + name + "\" has multiple definitions: " + existing.getLocation() + " and " + descriptor.getLocation());
                 }
             }
-        }
+            if (!(descriptor.getOptionKey() instanceof HostedOptionKey)) {
+                OptionDescriptor existing = allRuntimeOptions.put(name, descriptor);
+                if (existing != null) {
+                    throw shouldNotReachHere("Option name \"" + name + "\" has multiple definitions: " + existing.getLocation() + " and " + descriptor.getLocation());
+                }
+            }
+        });
     }
 
     public List<String> parse(List<String> args) {

lusou-zhangquan/ziyi-forked-graal

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/option/HostedOptionParser.java
@@ -24,30 +24,27 @@
  */
 package com.oracle.svm.hosted.option;
 
-import static com.oracle.svm.core.option.SubstrateOptionsParser.BooleanOptionFormat.PLUS_MINUS;
-import static com.oracle.svm.core.util.VMError.shouldNotReachHere;
-
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.graalvm.collections.EconomicMap;
-import org.graalvm.collections.EconomicSet;
-import org.graalvm.compiler.options.OptionDescriptor;
-import org.graalvm.compiler.options.OptionDescriptors;
-import org.graalvm.compiler.options.OptionDescriptorsMap;
-import org.graalvm.compiler.options.OptionKey;
-import org.graalvm.compiler.options.OptionValues;
-import org.graalvm.nativeimage.Platforms;
-
 import com.oracle.svm.core.option.HostedOptionKey;
 import com.oracle.svm.core.option.RuntimeOptionKey;
 import com.oracle.svm.core.option.SubstrateOptionsParser;
 import com.oracle.svm.core.util.InterruptImageBuilding;
 import com.oracle.svm.core.util.UserError;
 import com.oracle.svm.hosted.ImageClassLoader;
+import org.graalvm.collections.EconomicMap;
+import org.graalvm.collections.EconomicSet;
+import org.graalvm.compiler.options.OptionDescriptor;
+import org.graalvm.compiler.options.OptionDescriptors;
+import org.graalvm.compiler.options.OptionKey;
+import org.graalvm.compiler.options.OptionValues;
+import org.graalvm.nativeimage.Platforms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.oracle.svm.common.option.CommonOptionParser.BooleanOptionFormat.PLUS_MINUS;
+import static com.oracle.svm.core.util.VMError.shouldNotReachHere;
 
 public class HostedOptionParser implements HostedOptionProvider {
 
@@ -62,39 +59,26 @@ public class HostedOptionParser implements HostedOptionProvider {
 
     public static void collectOptions(List<Class<? extends OptionDescriptors>> optionsClasses, EconomicMap<String, OptionDescriptor> allHostedOptions,
                     EconomicMap<String, OptionDescriptor> allRuntimeOptions) {
-        for (Class<? extends OptionDescriptors> optionsClass : optionsClasses) {
-            if (Modifier.isAbstract(optionsClass.getModifiers()) || OptionDescriptorsMap.class.isAssignableFrom(optionsClass)) {
-                continue;
+        SubstrateOptionsParser.collectOptions(optionsClasses, descriptor -> {
+            String name = descriptor.getName();
+            if (descriptor.getDeclaringClass().getAnnotation(Platforms.class) != null) {
+                throw UserError.abort("Options must not be declared in a class that has a @%s annotation: option %s declared in %s",
+                        Platforms.class.getSimpleName(), name, descriptor.getDeclaringClass().getTypeName());
             }
 
-            OptionDescriptors descriptors;
-            try {
-                descriptors = optionsClass.getDeclaredConstructor().newInstance();
-            } catch (Exception ex) {
-                throw shouldNotReachHere(ex);
-            }
-            for (OptionDescriptor descriptor : descriptors) {
-                String name = descriptor.getName();
-
-                if (descriptor.getDeclaringClass().getAnnotation(Platforms.class) != null) {
-                    throw UserError.abort("Options must not be declared in a class that has a @%s annotation: option %s declared in %s",
-                                    Platforms.class.getSimpleName(), name, descriptor.getDeclaringClass().getTypeName());
-                }
-
-                if (!(descriptor.getOptionKey() instanceof RuntimeOptionKey)) {
-                    OptionDescriptor existing = allHostedOptions.put(name, descriptor);
-                    if (existing != null) {
-                        throw shouldNotReachHere("Option name \"" + name + "\" has multiple definitions: " + existing.getLocation() + " and " + descriptor.getLocation());
-                    }
-                }
-                if (!(descriptor.getOptionKey() instanceof HostedOptionKey)) {
-                    OptionDescriptor existing = allRuntimeOptions.put(name, descriptor);
-                    if (existing != null) {
-                        throw shouldNotReachHere("Option name \"" + name + "\" has multiple definitions: " + existing.getLocation() + " and " + descriptor.getLocation());
-                    }
+            if (!(descriptor.getOptionKey() instanceof RuntimeOptionKey)) {
+                OptionDescriptor existing = allHostedOptions.put(name, descriptor);
+                if (existing != null) {
+                    throw shouldNotReachHere("Option name \"" + name + "\" has multiple definitions: " + existing.getLocation() + " and " + descriptor.getLocation());
                 }
             }
-        }
+            if (!(descriptor.getOptionKey() instanceof HostedOptionKey)) {
+                OptionDescriptor existing = allRuntimeOptions.put(name, descriptor);
+                if (existing != null) {
+                    throw shouldNotReachHere("Option name \"" + name + "\" has multiple definitions: " + existing.getLocation() + " and " + descriptor.getLocation());
+                }
+            }
+        });
     }
 
     public String[] parse(String[] args) {